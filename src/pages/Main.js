105df043fca5d8b4bb4fb352592bd5ce748ac3f1
gusgusz <gustavoguimaraescamps@gmail.com>
2022-11-20 21:11:47 -0300

    sign up and sign in done

---
+++ src/pages/Main.js
@@ -0,0 +1,32 @@
+import Logo from "../components/Logo/Logo.js";
+import {useNavigate} from "react-router-dom";
+import {useContext} from "react";
+import {Container, Input, Button, Form, StyledLink} from "../styles/styles.js";
+import { Link } from "react-router-dom";
+import
+
+
+import { useState, useEffect } from "react";
+
+export default function Main() {
+  
+   const [transactions, setTransactions] = useState([]);
+    
+  
+ 
+  
+    return (
+      <Container>
+        <Top>
+        <div>Olá, Gustavo!!!</div>
+        <ion-icon name="exit-outline"></ion-icon>
+        </Top>
+        
+  
+      </Container>
+    );};
+
+
+
+
+
